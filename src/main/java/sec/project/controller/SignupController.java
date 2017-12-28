aaecfdb89035b9f38e868d4a6e1f3acace209df1
Mika Tavi <mtavi@yahoo.com>
2017-12-28 20:58:11 +0200

    Version 1

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -1,7 +1,10 @@
 package sec.project.controller;
 
+import javax.annotation.PostConstruct;
+import org.hibernate.criterion.Example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,20 +16,36 @@ public class SignupController {
 
     @Autowired
     private SignupRepository signupRepository;
+    
+    @PostConstruct
+    public void init() {
+        // test data to the application
+        Signup signup = new Signup("bob none", "bobs address");
+        signupRepository.save(signup);
+    }
 
     @RequestMapping("*")
     public String defaultMapping() {
+        System.out.println("defaultmapping");
         return "redirect:/form";
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.GET)
-    public String loadForm() {
+    public String loadForm(Model model) {
+        model.addAttribute("signups", signupRepository.findAll());
+        System.out.println("GET "+signupRepository.findAll().toString());
         return "form";
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String submitForm(@RequestParam String name, @RequestParam String address) {
-        signupRepository.save(new Signup(name, address));
+    public String submitForm(@RequestParam String name, @RequestParam String address, Model model) {
+        System.out.println("name: "+name);
+        System.out.println("address: "+address);
+        Signup signup=new Signup(name, address);
+        signupRepository.save(signup);
+        Long id = signup.getId();
+        System.out.println("GET "+signupRepository.findOne(id));
+        model.addAttribute("signups", signupRepository.findOne(id));
         return "done";
     }
 
